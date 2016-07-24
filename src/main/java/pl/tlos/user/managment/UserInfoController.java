514b159316f1ba4c72d76cb79a56649c6a60b793
Tomasz Los <tloszabno@gmail.com>
2016-07-24 20:49:44 +0200

    Changed controller to async

--- src/main/java/pl/tlos/user/managment/UserInfoController.java
+++ src/main/java/pl/tlos/user/managment/UserInfoController.java
@@ -1,13 +1,15 @@
 package pl.tlos.user.managment;
 
+import java.util.concurrent.Callable;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import pl.tlos.logger.Loggable;
+import pl.tlos.logger.Loggers;
 
 @Controller
 public class UserInfoController {
@@ -25,10 +27,12 @@ public class UserInfoController {
 	@RequestMapping("/user/saveAsync")
 	@ResponseBody
 	@Loggable
-	@Async
-	public String saveAsync(@RequestParam String name) {
+	public Callable<String> saveAsync(@RequestParam String name) {
 		userSLO.saveUser(name);
-		return "OK";
+		return () -> {
+			Loggers.forThis().warn("saveAsync -> " + Thread.currentThread().getName());
+			return "OK";
+		};
 	}
 
 }
